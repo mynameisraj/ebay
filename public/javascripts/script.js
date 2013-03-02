2bbd6ad1ef09eea8760e000d9d3d3f505ad6d518
Raj Ramamurthy <raj@mynameisraj.com>
2013-03-02 02:03:47 -0600

    Selective reload

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,6 +3,27 @@ var init = function() {
 		e.preventDefault();
 		refresh();
 	}, false);
+	
+	document.getElementById("top-pick").addEventListener("click", function(e) {
+		e.preventDefault();
+		pick("top");
+	}, false);
+	
+	document.getElementById("bottom-pick").addEventListener("click", function(e) {
+		e.preventDefault();
+		pick("bottom");
+	}, false);
+	
+	document.getElementById("shoe-pick").addEventListener("click", function(e) {
+		e.preventDefault();
+		pick("shoe");
+	}, false);
+	
+	document.getElementById("accessory-pick").addEventListener("click", function(e) {
+		e.preventDefault();
+		pick("accessory");
+	}, false);
+	
 	refresh();
 }
 
@@ -13,7 +34,7 @@ var refresh = function() {
 	var req = new XMLHttpRequest();
 	var top = document.getElementsByClassName("top-img")[0];
 	var bottom = document.getElementsByClassName("bottom-img")[0];
-	var shoes = document.getElementsByClassName("shoes-img")[0];
+	var shoe = document.getElementsByClassName("shoe-img")[0];
 	var accessory = document.getElementsByClassName("accessory-img")[0];
 	
 	req.onload = function() {
@@ -23,14 +44,36 @@ var refresh = function() {
 		top.getElementsByTagName("img")[0].setAttribute("src", r.top.src);
 		bottom.getElementsByTagName("a")[0].setAttribute("href", r.bottom.link);
 		bottom.getElementsByTagName("img")[0].setAttribute("src", r.bottom.src);
-		shoes.getElementsByTagName("a")[0].setAttribute("href", r.shoe.link);
-		shoes.getElementsByTagName("img")[0].setAttribute("src", r.shoe.src);
+		shoe.getElementsByTagName("a")[0].setAttribute("href", r.shoe.link);
+		shoe.getElementsByTagName("img")[0].setAttribute("src", r.shoe.src);
 		accessory.getElementsByTagName("a")[0].setAttribute("href", r.accessory.link);
 		accessory.getElementsByTagName("img")[0].setAttribute("src", r.accessory.src);
 		loading.style["display"] = "none";
 	};
 	var gender = document.getElementsByClassName("gender-sel")[0].value;
-	var url = "/test?gender=" + gender;
+	var url = "/refresh?gender=" + gender;
+	console.log(url);
+	req.open("get", url, true);
+	req.send();
+}
+
+var pick = function(kind) {
+	var loading = document.getElementsByClassName("loading")[0];
+	loading.style["display"] = "inline-block";
+
+	var req = new XMLHttpRequest();
+	var kindClass = kind + "-img";
+	var newEl = document.getElementsByClassName(kindClass)[0];
+	
+	req.onload = function() {
+		var r = JSON.parse(this.responseText);
+		console.log(r);
+		newEl.getElementsByTagName("a")[0].setAttribute("href", r.link);
+		newEl.getElementsByTagName("img")[0].setAttribute("src", r.src);
+		loading.style["display"] = "none";
+	};
+	var gender = document.getElementsByClassName("gender-sel")[0].value;
+	var url = "/pick?gender=" + gender + "&kind=" + kind;
 	console.log(url);
 	req.open("get", url, true);
 	req.send();
--- server.js
+++ server.js
@@ -6,17 +6,16 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
-app.get('/test', function (req, res) {
-	res.setHeader('Content-Type', 'application/json');
+app.get('/refresh', function (req, res) {
+	console.log("refreshing");
 	
+	res.setHeader('Content-Type', 'application/json');
 	var gender = "mens";
 	if (req.query.gender) gender = req.query.gender;
-	
 	var query = {
 		query: "",
 		sort_by: "trending_score",
 	};
-	
 	var finalres = {
 		top: {
 			link: "",
@@ -36,44 +35,89 @@ app.get('/test', function (req, res) {
 		}
 	};
 	
+	// get top
 	api.shop.products(gender, "clothing/tops/shirts", query, function(response) {
+		console.log("-top");
 		var index = Math.floor(Math.random()*response.response.products.length);
 		var product = response.response.products[index];
 		finalres.top.link = product.page_url;
 		finalres.top.src = product.image;
 		
+		// get bottom
 		var bottomopts = ["pants", "jeans"];
 		var bottom = "clothing/bottoms/" + bottomopts[Math.floor(Math.random()*bottomopts.length)];
-		
 		api.shop.products(gender, bottom, query, function(response) {
+			console.log("-bottom");
 			var index = Math.floor(Math.random()*response.response.products.length);
 			var product = response.response.products[index];
 			finalres.bottom.link = product.page_url;
 			finalres.bottom.src = product.image;
 			
+			// get shoes
 			api.shop.products(gender, "shoes", query, function(response) {
+				console.log("-shoe");
 				var index = Math.floor(Math.random()*response.response.products.length);
 				var product = response.response.products[index];
-				console.log(product);
 				finalres.shoe.link = product.page_url;
 				finalres.shoe.src = product.image;
 				
+				// get accessories
 				api.shop.products(gender, "accessories", query, function(response) {
+					console.log("-accessory");
 					var index = Math.floor(Math.random()*response.response.products.length);
 					var product = response.response.products[index];
-					console.log(product);
 					finalres.accessory.link = product.page_url;
 					finalres.accessory.src = product.image;
 					
-					console.log(finalres);
-					
 					res.send(JSON.stringify(finalres));
+					console.log("done");
 				});				
 			});
 		});
 	});
 });
 
+app.get('/pick', function (req, res) {
+	console.log("pick");
+	res.setHeader('Content-Type', 'application/json');
+	var finalres = {};
+	
+	var gender = "mens";
+	if (req.query.gender) gender = req.query.gender;
+	
+	var kind = "top";
+	if (req.query.kind) kind = req.query.kind;
+	
+	console.log("-" + kind);
+	if (kind.match("top")) {
+		kind = "clothing/tops/shirts";
+	} else if (kind.match("bottom")) {
+		var bottomopts = ["pants", "jeans"];
+		kind = "clothing/bottoms/" + bottomopts[Math.floor(Math.random()*bottomopts.length)];
+	} else if (kind.match("shoe")) {
+		kind = "shoes";
+	} else {
+		kind = "accessories";
+	}
+
+	var query = {
+		query: "",
+		sort_by: "trending_score",
+	};
+	
+	api.shop.products(gender, kind, query, function(response) {
+		var index = Math.floor(Math.random()*response.response.products.length);
+		var product = response.response.products[index];
+		finalres.link = product.page_url;
+		finalres.src = product.image;
+		
+		res.send(JSON.stringify(finalres));
+		console.log("done");
+	});
+});
+
+
+// port
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
