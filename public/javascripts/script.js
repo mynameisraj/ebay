9302ef235fa445984e8cefb773db04a691703228
Raj Ramamurthy <raj@mynameisraj.com>
2013-03-02 02:54:31 -0600

    Enable URL sharing

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,62 @@
+window.shareURL = window.location.href;
+window.ids = {
+	top: "",
+	bottom: "",
+	shoe: "",
+	accessory: ""
+};
+
+function getParameterByName(name)
+{
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.search);
+  if(results == null)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+// http://stackoverflow.com/questions/979975/how-to-get-the-value-from-url-parameter
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    	// If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = pair[1];
+    	// If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]], pair[1] ];
+      query_string[pair[0]] = arr;
+    	// If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(pair[1]);
+    }
+  } 
+    return query_string;
+} ();
+
+var updateURL = function() {
+	var url = encodeURI(window.location.href.split("?")[0] + "?top=" + window.ids.top + "&bottom=" + window.ids.bottom + "&shoe=" + window.ids.shoe + "&accessory=" + window.ids.accessory);
+	var textfield = document.getElementsByClassName("url")[0];
+	textfield.setAttribute("value", url);
+	return url;
+};
+
 var init = function() {
+	if (getParameterByName("top") && getParameterByName("bottom") && getParameterByName("shoe") && getParameterByName("accessory"))
+	{
+		refreshGiven(getParameterByName("top"), getParameterByName("bottom"), getParameterByName("shoe"), getParameterByName("accessory"));
+	} else {
+		refresh();
+	}
+
 	document.getElementById("refresh-btn").addEventListener("click", function(e) {
 		e.preventDefault();
 		refresh();
@@ -23,9 +81,7 @@ var init = function() {
 		e.preventDefault();
 		pick("accessory");
 	}, false);
-	
-	refresh();
-}
+};
 
 var refresh = function() {
 	var loading = document.getElementsByClassName("loading")[0];
@@ -48,6 +104,11 @@ var refresh = function() {
 		shoe.getElementsByTagName("img")[0].setAttribute("src", r.shoe.src);
 		accessory.getElementsByTagName("a")[0].setAttribute("href", r.accessory.link);
 		accessory.getElementsByTagName("img")[0].setAttribute("src", r.accessory.src);
+		
+		for (i in window.ids) {
+			window.ids[i] = r[i].id;
+		}
+		updateURL();
 		loading.style["display"] = "none";
 	};
 	var gender = document.getElementsByClassName("gender-sel")[0].value;
@@ -55,7 +116,41 @@ var refresh = function() {
 	console.log(url);
 	req.open("get", url, true);
 	req.send();
-}
+};
+
+var refreshGiven = function(topNum, bottomNum, shoeNum, accessoryNum) {
+	var loading = document.getElementsByClassName("loading")[0];
+	loading.style["display"] = "inline-block";
+
+	var req = new XMLHttpRequest();
+	var top = document.getElementsByClassName("top-img")[0];
+	var bottom = document.getElementsByClassName("bottom-img")[0];
+	var shoe = document.getElementsByClassName("shoe-img")[0];
+	var accessory = document.getElementsByClassName("accessory-img")[0];
+	
+	req.onload = function() {
+		var r = JSON.parse(this.responseText);
+		console.log(r);
+		top.getElementsByTagName("a")[0].setAttribute("href", r.top.link);
+		top.getElementsByTagName("img")[0].setAttribute("src", r.top.src);
+		bottom.getElementsByTagName("a")[0].setAttribute("href", r.bottom.link);
+		bottom.getElementsByTagName("img")[0].setAttribute("src", r.bottom.src);
+		shoe.getElementsByTagName("a")[0].setAttribute("href", r.shoe.link);
+		shoe.getElementsByTagName("img")[0].setAttribute("src", r.shoe.src);
+		accessory.getElementsByTagName("a")[0].setAttribute("href", r.accessory.link);
+		accessory.getElementsByTagName("img")[0].setAttribute("src", r.accessory.src);
+		
+		for (i in window.ids) {
+			window.ids[i] = r[i].id;
+		}
+		updateURL();
+		loading.style["display"] = "none";
+	};
+	var url = "/refreshgiven?top=" + topNum + "&bottom=" + bottomNum + "&shoe=" + shoeNum + "&accessory=" + accessoryNum;
+	console.log(url);
+	req.open("get", url, true);
+	req.send();
+};
 
 var pick = function(kind) {
 	var loading = document.getElementsByClassName("loading")[0];
@@ -70,6 +165,8 @@ var pick = function(kind) {
 		console.log(r);
 		newEl.getElementsByTagName("a")[0].setAttribute("href", r.link);
 		newEl.getElementsByTagName("img")[0].setAttribute("src", r.src);
+		window.ids[kind] = r.id;
+		updateURL();
 		loading.style["display"] = "none";
 	};
 	var gender = document.getElementsByClassName("gender-sel")[0].value;
@@ -77,6 +174,6 @@ var pick = function(kind) {
 	console.log(url);
 	req.open("get", url, true);
 	req.send();
-}
+};
 
 window.addEventListener("load", init, false);
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
+// refresh all
 app.get('/refresh', function (req, res) {
 	console.log("refreshing");
 	
@@ -19,19 +20,23 @@ app.get('/refresh', function (req, res) {
 	var finalres = {
 		top: {
 			link: "",
-			src: ""
+			src: "",
+			id: ""
 		},
 		bottom: {
 			link: "",
-			src: ""
+			src: "",
+			id: ""
 		},
 		shoe: {
 			link: "",
-			src: ""
+			src: "",
+			id: ""
 		},
 		accessory: {
 			link: "",
-			src: ""
+			src: "",
+			id: ""
 		}
 	};
 	
@@ -42,6 +47,7 @@ app.get('/refresh', function (req, res) {
 		var product = response.response.products[index];
 		finalres.top.link = product.page_url;
 		finalres.top.src = product.image;
+		finalres.top.id = product.id_str;
 		
 		// get bottom
 		var bottomopts = ["pants", "jeans"];
@@ -52,6 +58,7 @@ app.get('/refresh', function (req, res) {
 			var product = response.response.products[index];
 			finalres.bottom.link = product.page_url;
 			finalres.bottom.src = product.image;
+			finalres.bottom.id = product.id_str;
 			
 			// get shoes
 			api.shop.products(gender, "shoes", query, function(response) {
@@ -60,6 +67,7 @@ app.get('/refresh', function (req, res) {
 				var product = response.response.products[index];
 				finalres.shoe.link = product.page_url;
 				finalres.shoe.src = product.image;
+				finalres.shoe.id = product.id_str;
 				
 				// get accessories
 				api.shop.products(gender, "accessories", query, function(response) {
@@ -68,6 +76,7 @@ app.get('/refresh', function (req, res) {
 					var product = response.response.products[index];
 					finalres.accessory.link = product.page_url;
 					finalres.accessory.src = product.image;
+					finalres.accessory.id = product.id_str;
 					
 					res.send(JSON.stringify(finalres));
 					console.log("done");
@@ -77,6 +86,7 @@ app.get('/refresh', function (req, res) {
 	});
 });
 
+// chooses specific one to refresh
 app.get('/pick', function (req, res) {
 	console.log("pick");
 	res.setHeader('Content-Type', 'application/json');
@@ -110,12 +120,86 @@ app.get('/pick', function (req, res) {
 		var product = response.response.products[index];
 		finalres.link = product.page_url;
 		finalres.src = product.image;
+		finalres.id = product.id_str;
 		
 		res.send(JSON.stringify(finalres));
 		console.log("done");
 	});
 });
 
+// refreshes with given ids
+app.get('/refreshgiven', function (req, res) {
+	console.log("refreshgiven");
+	res.setHeader('Content-Type', 'application/json');
+	var finalres = {};
+	
+	var query = {
+		query: "",
+		sort_by: "trending_score",
+	};
+	
+	var finalres = {
+		top: {
+			link: "",
+			src: "",
+			id: ""
+		},
+		bottom: {
+			link: "",
+			src: "",
+			id: ""
+		},
+		shoe: {
+			link: "",
+			src: "",
+			id: ""
+		},
+		accessory: {
+			link: "",
+			src: "",
+			id: ""
+		}
+	};
+	
+	// get top
+	api.products.show(parseInt(req.query.top), function(response) {
+		console.log("-top");
+		var product = response.response.product;
+		finalres.top.link = product.page_url;
+		finalres.top.src = product.image;
+		finalres.top.id = product.id_str;
+		
+		// bottom
+		api.products.show(parseInt(req.query.bottom), function(response) {
+			console.log("-bottom");
+			var product = response.response.product;
+			finalres.bottom.link = product.page_url;
+			finalres.bottom.src = product.image;
+			finalres.bottom.id = product.id_str;
+			
+			// shoe
+			api.products.show(parseInt(req.query.shoe), function(response) {
+				console.log("-shoe");
+				var product = response.response.product;
+				finalres.shoe.link = product.page_url;
+				finalres.shoe.src = product.image;
+				finalres.shoe.id = product.id_str;
+				
+				// accessory
+				api.products.show(parseInt(req.query.accessory), function(response) {
+					console.log("-shoe");
+					var product = response.response.product;
+					finalres.accessory.link = product.page_url;
+					finalres.accessory.src = product.image;
+					finalres.accessory.id = product.id_str;
+				
+					res.send(JSON.stringify(finalres));
+					console.log("done");
+				});
+			});
+		});
+	});
+});
 
 // port
 var port = process.env.PORT || 5000;
